1bb0ca14e56ba03ba228102d48f8d39597bc38fc
hugomunozsantana <95626271+hugomunozsantana@users.noreply.github.com>
2022-01-06 15:24:14 -0300

    Create demo_json.php

---
+++ demo_json.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<?php
+$json=$_GET['x'];
+
+?>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>	
+<body>
+	<h1>Mi PHP recibe json</h1>
+	<p>
+		<?php echo "json:".$json; ?>
+	</p>
+</body>
+</html>
